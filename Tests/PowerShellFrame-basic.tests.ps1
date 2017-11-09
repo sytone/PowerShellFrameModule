3adacd1aa833a596526a0caa3bbf1bd86374ce4c
Sytone <github@sytone.com>
2017-11-08 16:35:07 -0800

    Fix module name as github project nad is not the same as module name.

--- Tests/PowerShellFrame-basic.tests.ps1
+++ Tests/PowerShellFrame-basic.tests.ps1
@@ -4,14 +4,14 @@ if($env:APPVEYOR_REPO_BRANCH -and $env:APPVEYOR_REPO_BRANCH -notlike "master") {
 }
 
 $moduleRoot = Resolve-Path "$PSScriptRoot\.."
-$moduleName = Split-Path $moduleRoot -Leaf
+$moduleName = "PowerShellFrame"
 
 Describe "General project validation: $moduleName" {
 
     $scripts = Get-ChildItem $moduleRoot -Include *.ps1, *.psm1, *.psd1 -Recurse
 
     # TestCases are splatted to the script so we need hashtables
-    $testCase = $scripts | Foreach-Object {@{file = $_}}         
+    $testCase = $scripts | Foreach-Object {@{file = $_}}
     It "Script <file> should be valid powershell" -TestCases $testCase {
         param($file)
 

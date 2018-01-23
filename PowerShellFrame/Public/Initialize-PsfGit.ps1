c5668c945c888b29a19ff58af40b97f69c457030
Sytone <sytone@users.noreply.github.com>
2018-01-23 14:36:59 -0800

    Added PowerShellFrameModule to the init helper command.

--- PowerShellFrame/Public/Initialize-PsfGit.ps1
+++ PowerShellFrame/Public/Initialize-PsfGit.ps1
@@ -3,4 +3,8 @@ function Initialize-PsfGit() {
     $cloneRoot = (Join-Path (Get-PsfConfig -Key DevelopmentFolder) "PowerShellFrame")
     if(!(Test-Path $cloneRoot)) {New-Item -Path $cloneRoot -ItemType Directory | Out-Null }
     git clone "https://github.com/sytone/PowerShellFrame.git" $cloneRoot
+    
+    $cloneRoot = (Join-Path (Get-PsfConfig -Key DevelopmentFolder) "PowerShellFrameModule")
+    if(!(Test-Path $cloneRoot)) {New-Item -Path $cloneRoot -ItemType Directory | Out-Null }
+    git clone "https://github.com/sytone/PowerShellFrameModule.git" $cloneRoot    
   }
